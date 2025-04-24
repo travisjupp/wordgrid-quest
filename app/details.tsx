e22915335e9c6aabd20216839f936ae7c54b1237
tjupp <travisjupp@gmail.com>
2025-04-24 07:00:02 -0600

    Update app config/layout

---
+++ app/details.tsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Details `app/details.tsx`</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(19 37 64)',
+  },
+});
+
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { Link } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+    <Text style={styles.text}>Home `app/index.tsx`</Text>
+      <Link style={styles.link} href="/details">View details</Link>
+      <Link style={styles.link} href="/flex">View flex</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(19 37 64)'
+  },
+  text: {
+    color: 'purple',
+    fontSize: 45
+  },
+  link: {
+    color: 'cornflowerblue',
+    fontVariant: 'bold',
+    fontSize: 26
+  }
+});
+
