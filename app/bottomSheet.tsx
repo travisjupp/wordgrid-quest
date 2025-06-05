e2a915b8c54112e0b4b6ac4d115bd4765980b52f
tjupp <travisjupp@gmail.com>
2025-06-05 09:37:19 -0600

    Test bottom sheet config using index
    The technique using the index property for bottom sheet does not trigger
    the sheet. Consider using bottom sheet methods only.

--- app/bottomSheet.tsx
+++ app/bottomSheet.tsx
@@ -10,8 +10,8 @@ export default function App() {
         <Button title="Toggle Sheet" onPress={() => setIsOpen(o => !o)} />
         <BottomSheet
           snapPoints={['50%', '75%']}
-          // index={isOpen ? 0 : -1}
-          index={0}
+          index={isOpen ? 0 : -1}
+          // index={0}
           onChange={idx => setIsOpen(idx !== -1)}
         >
           <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
