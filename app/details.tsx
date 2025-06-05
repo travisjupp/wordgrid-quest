125e81e9a74563fe0950a0beeba0feb425d83e1b
tjupp <travisjupp@gmail.com>
2025-06-05 08:51:38 -0600

    Remove redirect to missing flex route/file

--- app/details.tsx
+++ app/details.tsx
@@ -1,10 +1,8 @@
 import { View, Text, StyleSheet } from 'react-native';
-import { Redirect } from 'expo-router';
 
 export default function DetailsScreen() {
   return (
     <View style={styles.container}>
-      <Redirect href="/flex" />
       <Text>Details `app/details.tsx`</Text>
     </View>
   );
