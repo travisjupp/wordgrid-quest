c831e592a360e6b7b6518ae06dc46fc2cc312601
tjupp <travisjupp@gmail.com>
2025-06-05 09:35:12 -0600

    Move React Native Paper provider to project root

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,62 @@
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Platform } from 'react-native';
+import * as React from 'react';
+import { Text, MD3LightTheme as DefaultTheme, MD3DarkTheme, configureFonts,
+  FAB, Menu, Divider, PaperProvider, Button } from 'react-native-paper';
+
+const fontConfig = {
+  // fontFamily: 'Abel', // override all variants (only if no variants)
+  default: {
+    fontSize: 12,
+    // fontFamily: 'sans-serif',
+    fontFamily: Platform.select({
+      web: 'Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif',
+      // ios: 'Default',
+      ios: 'System',
+      default: 'sans-serif',
+    }),
+    fontWeight: undefined,
+    letterSpacing: 0,
+    lineHeight: 20,
+  },
+};
 export default function RootLayout() {
+  const [isThemeDark, setIsThemeDark] = React.useState(true);
+  const theme = isThemeDark ? {
+    ...MD3DarkTheme,
+    fonts: configureFonts({config: fontConfig}),
+    colors: {
+      ...MD3DarkTheme.colors,
+      // primary: 'tomato',
+      // secondary: 'yellow',
+    },
+  } : {
+      ...DefaultTheme,
+      fonts: configureFonts({config: fontConfig}),
+      colors: {
+        ...DefaultTheme.colors,
+        // primary: 'tomato',
+        // secondary: 'yellow',
+      },
+    }
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-    </Stack>
-    </GestureHandlerRootView>
+    <PaperProvider theme={theme}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}>
+        </Stack>
+      </GestureHandlerRootView>
+    </PaperProvider>
   );
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -2,85 +2,77 @@ import 'react-native-gesture-handler';
 import 'react-native-reanimated';
 import * as React from 'react';
 import { Link } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
-import { MD3DarkTheme as DefaultTheme, 
-  FAB, Menu, Divider, PaperProvider, Button } from 'react-native-paper';
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'tomato',
-    // secondary: 'yellow',
-  },
-};
+import { View, StyleSheet, Platform } from 'react-native';
+import { useTheme, Text, FAB, Menu, Divider, Button } from 'react-native-paper';
 
 export default function HomeScreen() {
   const [visible, setVisible] = React.useState(false);
   const openMenu = () => setVisible(true);
   const closeMenu = () => setVisible(false);
-
+  const theme = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      // backgroundColor: 'rgb(19 37 64)',
+      backgroundColor: 'theme.colors.surface',
+    },
+    text: {
+      color: 'purple',
+      // fontSize: 45
+    },
+    link: {
+      color: theme.colors.onSurface,
+      fontWeight: 'bold',
+      fontSize: 26
+    },
+    fab: {
+      // flex: 1,
+      // justifyContent: 'center',
+      // alignItems: 'center',
+      // width: '50%',
+    },
+  });
   return (
-    <PaperProvider theme={theme}>
-      <View
-        style={styles.container}>
-        <Menu
-          visible={visible}
-          onDismiss={closeMenu}
-          anchor={<Button icon="dots-vertical" onPress={openMenu}>Show menu
-          </Button>}>
-          <Menu.Item onPress={() => {}} title="Item 1" />
-          <Menu.Item onPress={() => {}} title="Item 2" />
-          <Divider />
-          <Menu.Item onPress={() => {}} title="Item 3" />
-        </Menu>
-        <FAB
-          icon="ladybug"
-          style={styles.fab}
-          onPress={() => console.log('Pressed')}
-        />
-        <FAB
-          icon="bird"
-          style={styles.fab}
-          onPress={() => console.log('Pressed')}
-        />
-        <FAB
-          icon="skull-outline"
-          style={styles.fab}
-          onPress={() => console.log('Pressed')}
-        />
-        <Text style={styles.text}>Home `app/index.tsx`</Text>
-        <Button icon="camera" mode="contained">SSS</Button>
-        <Button icon="camera" disabled={true} mode="contained">Disabled</Button>
-        <Link style={styles.link} href="/details">View details</Link>
-        <Link style={styles.link} href="/flex">View flex</Link>
-        <Link style={styles.link} href="/bottomSheet">View bottomSheet</Link>
-      </View>
-    </PaperProvider>
+    <View
+      // style={styles.container}
+      style={{ backgroundColor: theme.colors.surface}}
+      //style={{ backgroundColor: '#ff00ff'}}
+    >
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={<Button icon="dots-vertical" onPress={openMenu}>Show menu
+        </Button>}>
+        <Menu.Item onPress={() => {}} title="Item 1" />
+        <Menu.Item onPress={() => {}} title="Item 2" />
+        <Divider />
+        <Menu.Item onPress={() => {}} title="Item 3" />
+      </Menu>
+      <FAB
+        icon="ladybug"
+        style={styles.fab}
+        onPress={() => console.log('Pressed')}
+      />
+      <FAB
+        icon="bird"
+        style={styles.fab}
+        onPress={() => console.log('Pressed')}
+      />
+      <FAB
+        icon="skull-outline"
+        style={styles.fab}
+        onPress={() => console.log('Pressed')}
+      />
+      <Text variant="bodyLarge">Home `app/index.tsx`</Text>
+      <Text variant="displayLarge" style={styles.text}>STYLE Text</Text>
+      <Button icon="camera" mode="contained">SSS</Button>
+      <Button icon="camera" disabled={true} mode="contained">Disabled</Button>
+      <Link style={{color: theme.colors.onSurface}} href="/details">View details</Link>
+      <Link style={styles.link} href="/bottomSheet">View bottomSheet</Link>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgb(19 37 64)',
-  },
-  text: {
-    color: 'purple',
-    fontSize: 45
-  },
-  link: {
-    color: 'cornflowerblue',
-    fontWeight: 'bold',
-    fontSize: 26
-  },
-  fab: {
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // width: '50%',
-  },
-});
 
