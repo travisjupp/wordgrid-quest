384879decd2fb79922737eccdfce4e0f65bdf5e4
tjupp <travisjupp@gmail.com>
2025-07-03 13:24:49 -0600

    Remove unused imports and vars

--- app/details.tsx
+++ app/details.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import { Text } from '@/components/Text';
-import ThemeContext from '@/contexts/ThemeContext';
+// import ThemeContext from '@/contexts/ThemeContext';
 import { useAppTheme } from '@/app/_layout';
 
 export default function DetailsScreen() {
-  const { isDarkTheme, toggleTheme } = React.useContext(ThemeContext);
+  // const { isDarkTheme, toggleTheme } = React.useContext(ThemeContext);
   // Retrieve Custom Properties
   const {
     container,
-    text,
-    link,
+    // text,
+    // link,
     // colors: { brandPrimary },
   } = useAppTheme();
 
@@ -20,13 +20,4 @@ export default function DetailsScreen() {
     </View>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: 'rgb(19 37 64)',
-//   },
-// });
 
