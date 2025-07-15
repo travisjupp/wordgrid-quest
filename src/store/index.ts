2968463fe8b53129f647caf7dd30a870dd71c260
tjupp <travisjupp@gmail.com>
2025-07-15 14:48:50 -0600

    Update Redux Store creation to include store type
    - Updated description for #83

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: rootReducer,
   // Other store configs E.g., middleware, devtools, &c.
 });
 
+// Get the type of our store variable
+export type AppStore = typeof store;
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export default store;
 
