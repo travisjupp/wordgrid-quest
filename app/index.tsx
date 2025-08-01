a8b226c0fd6318dc7beafbac00bbfb143ff121f0
tjupp <travisjupp@gmail.com>
2025-08-01 10:43:41 -0600

    Test `MajorHUD` in index

--- app/index.tsx
+++ app/index.tsx
@@ -6,38 +6,20 @@ import { FAB, Button, Switch } from 'react-native-paper';
 import { Text } from '@components/Text';
 import ThemeContext from '@contexts/ThemeContext';
 import { useAppTheme } from '@theme/themeConfig';
-import Timer from '@features/timer/Timer';
-import TopicFrame from '@components/TopicFrame';
-// import * as useFonts from 'expo-font';
+import MajorHUD from '@/src/features/majorHUD/MajorHUD';
 
 export default function HomeScreen() {
   const { isDarkTheme, toggleTheme } = React.useContext(ThemeContext);
 
-// if (Platform.OS === "ios") {
-// console.log('loaded ios fonts', useFonts.getLoadedFonts());
-//   }
   // Retrieve Custom Theme-properties
   const {
     container,
     text,
   } = useAppTheme();
 
-  const definitionsArray = [
-    "This is the first paragraph. A little longer to see how it wraps.",
-    "Here is the second paragraph.  It's also quite informative and can be a bit lengthy to showcase the carousel's adaptability.",
-    "And finally, the third paragraph. This one provides a concise summary of the key points discussed earlier."
-  ];
-  const category = 'Asynchronous JS';
   return (
     <View style={container}>
       <Switch value={isDarkTheme} onValueChange={toggleTheme} />
-      <Timer />
-      <TopicFrame 
-        category={category}
-        definitions={definitionsArray}
-        topicframeWidth={200}
-        topicframeHeight={200}
-      />
       <FAB
         icon="skull-outline"
         onPress={() => console.log('Pressed')}
@@ -49,6 +31,7 @@ export default function HomeScreen() {
         style={{fontFamily: "Inter24pt-Black"}}
       >NO variant, inline style fontFamily: Inter24pt-Black</Text>
 
+      <MajorHUD />
       <Text 
         style={{fontFamily: "InriaSerif-Regular"}}
       >NO variant, inline style fontFamily: InriaSerif-Regular</Text>
