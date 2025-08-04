12547a2e8e9fc103528b93b9440a34795d4b9480
tjupp <travisjupp@gmail.com>
2025-08-04 13:05:17 -0600

    Create selector for Discovery Terms #96
    - Create selector for Discovery Terms for active Category

--- src/features/material/materialSelectors.ts
+++ src/features/material/materialSelectors.ts
@@ -1,5 +1,10 @@
 import type { RootState } from '../../store';
 
+export const selectDiscoveryTermsForActiveCategory = (state: RootState) => {
+  const customMaterialArray = state.material.category[state.material.activeCategory] || [];
+  return customMaterialArray.map(discoveryTermObject => discoveryTermObject.dt);
+};
+
 export const selectDefinitionsForActiveCategory = (state: RootState) => {
   const customMaterialArray = state.material.category[state.material.activeCategory] || [];
   return customMaterialArray.map(discoveryTermObject => discoveryTermObject.def);
