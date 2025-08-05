22140322381cb271bbeb2421972ebadab077a394
tjupp <travisjupp@gmail.com>
2025-08-05 09:56:36 -0600

    Write unit-tests for Material slices #109 #110
    - Tests created for testing `materialSlice` and `tempMaterialSlice`
      slices' reducer actions
    
    Closes #109
    Closes #110

---
+++ src/features/__tests__/materialSlice.test.ts
@@ -0,0 +1,68 @@
+import { InitialMaterialSliceState } from '@custom-types/AppTheme';
+import materialReducer, { addCustomCategory, setActiveCategory } from '@features/material/materialSlice';
+
+describe('materialSlice', () => {
+  it('should set the add custom category when addCustomCategory is dispatched', () => {
+    const initialState: InitialMaterialSliceState = {
+      category: {
+        Marsupials: [
+          { dt: 'Koala', def: 'A tree-dwelling marsupial native to Australia.' },
+          { dt: 'Wombat', def: 'A burrowing marsupial with a stubby tail.' },
+          { dt: 'Kangaroo', def: 'A large marsupial that hops and carries young in pouch.' }
+        ],
+        // User-Defined Custom-Material here
+      },
+      activeCategory: 'Marsupials',
+    };
+
+    const customCategory = {
+      category: 'Mammals',
+      discoveryTerms: [
+        { dt: 'Whale', def: 'Largest mammal on Earth' },
+        { dt: 'Shrew', def: 'Smallest mammal on Earth' },
+        { dt: 'Cheetah', def: 'Fastest mammal on Earth' }
+      ]
+    };
+
+    const expectedState = {
+      category: {
+        Marsupials: [
+          { dt: 'Koala', def: 'A tree-dwelling marsupial native to Australia.' },
+          { dt: 'Wombat', def: 'A burrowing marsupial with a stubby tail.' },
+          { dt: 'Kangaroo', def: 'A large marsupial that hops and carries young in pouch.' }
+        ],
+        Mammals: [
+          { dt: 'Whale', def: 'Largest mammal on Earth' },
+          { dt: 'Shrew', def: 'Smallest mammal on Earth' },
+          { dt: 'Cheetah', def: 'Fastest mammal on Earth' }
+        ]
+      },
+      activeCategory: 'Marsupials'
+    };
+
+    const action = addCustomCategory(customCategory);
+    const nextState = materialReducer(initialState, action);
+    expect(nextState).toStrictEqual(expectedState);
+  });
+
+  it('should set the active category when setActiveCategory is dispatched', () => {
+    const initialState: InitialMaterialSliceState = {
+      category: {
+        Marsupials: [
+          { dt: 'Koala', def: 'A tree-dwelling marsupial native to Australia.' },
+          { dt: 'Wombat', def: 'A burrowing marsupial with a stubby tail.' },
+          { dt: 'Kangaroo', def: 'A large marsupial that hops and carries young in pouch.' }
+        ],
+        // User-Defined Custom-Material here
+      },
+      activeCategory: 'Marsupials',
+    };
+
+    const action = setActiveCategory('Mammals');
+    const nextState = materialReducer(initialState, action);
+    expect(nextState.activeCategory).toBe('Mammals');
+
+  });
+
+});
+
---
+++ src/features/__tests__/tempMaterialSlice.test.ts
@@ -0,0 +1,46 @@
+import { InitialTempMaterialSliceState } from '@custom-types/AppTheme';
+import tempMaterialReducer, { setCategory, addItem, removeItem, resetTempMaterial } from '@features/tempMaterial/tempMaterialSlice';
+
+describe('tempMaterialSlice', () => {
+  it('should set the category when setCategory is dispatched', () => {
+    const initialState: InitialTempMaterialSliceState = { category: '' , discoveryTerms: [] };
+    const action = setCategory('Mammals');
+    const nextState = tempMaterialReducer(initialState, action);
+
+    expect(nextState.category).toBe('Mammals');
+    expect(nextState.discoveryTerms).toEqual([]);
+  });
+
+  it('should add item when addItem is dispatched', () => {
+    const initialState: InitialTempMaterialSliceState = { category: '' , discoveryTerms: [] };
+    const action = addItem({dt: 'Whale', def: 'Largest mammals on Earth'});
+    const nextState = tempMaterialReducer(initialState, action);
+
+    expect(nextState.category).toBe('');
+    expect(nextState.discoveryTerms)
+      .toEqual([{dt: 'Whale', def: 'Largest mammals on Earth'}]);
+  });
+
+  it('should remove item when removeItem is dispatched', () => {
+    const initialState: InitialTempMaterialSliceState = { category: 'Mammals' , 
+      discoveryTerms: [{dt: 'Whale', def: 'Largest mammals on Earth'}] };
+    const action = removeItem({dt: 'Whale', def: 'Largest mammals on Earth'});
+    const nextState = tempMaterialReducer(initialState, action);
+
+    expect(nextState.category).toBe('Mammals');
+    expect(nextState.discoveryTerms)
+      .toEqual([]);
+  });
+
+  it('should reset tempMaterial when resetTempMaterial is dispatched', () => {
+    const initialState: InitialTempMaterialSliceState = { category: 'Mammals' , 
+      discoveryTerms: [{dt: 'Whale', def: 'Largest mammals on Earth'}] };
+    const action = resetTempMaterial();
+    const nextState = tempMaterialReducer(initialState, action);
+
+    expect(nextState.category).toBe('');
+    expect(nextState.discoveryTerms).toEqual([]);
+  });
+
+});
+
