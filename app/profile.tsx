64fcef2a2dff96311f60841c55d3a7fbe7188b7e
tjupp <travisjupp@gmail.com>
2025-08-28 20:49:31 -0600

    Use react-native-reanimated for screen transitions
    - Transitions for screens on the web need reanimated

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
-import { View } from 'react-native';
 import { Text } from '@components/Text';
 import { useAppTheme } from '@theme/themeConfig';
+import Animated, { SlideInRight } from 'react-native-reanimated'; // Use SlideInRight or other entering animations
 
 export default function ProfileScreen() {
   // Retrieve Custom Properties
   const { container } = useAppTheme();
 
   return (
-    <View style={container}>
+    <Animated.View
+      entering={SlideInRight}
+      style={container}
+      testID='Animated.View'
+    >
       <Text variant='bodyLarge'>Profile `app/profile.tsx`</Text>
-    </View>
+    </Animated.View>
   );
 }
