bd1e1f2701589588c35e8e46cb8cad03cd4d8062
tjupp <travisjupp@gmail.com>
2025-09-08 11:07:14 -0600

    Remove old `createContext` call

--- src/contexts/LogoContext.ts
+++ src/contexts/LogoContext.ts
@@ -8,14 +8,6 @@ interface LogoContextType {
   scaleLogo: ScaleLogoType;
 }
 
-// const LogoContext = createContext({
-//   toggleLogo: () => {
-//     console.log('LogoContext not provided');
-//   },
-//   scaleLogo: (size?: number | undefined) => {
-//     console.log('LogoContext not provided');
-//   },
-// });
 const LogoContext = createContext<LogoContextType | null>(null);
 
 export default LogoContext;
