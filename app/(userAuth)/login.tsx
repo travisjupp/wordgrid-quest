8b518cca597df08ff7c49e48df00d93512a95355
tjupp <travisjupp@gmail.com>
2025-09-21 05:34:54 -0600

    Memoize sub-components for login and signup screen
    - Prevent unnecessary rerenders (related: #135)

--- app/(userAuth)/login.tsx
+++ app/(userAuth)/login.tsx
@@ -1,22 +1,27 @@
 import { LogIn } from '@components/LogIn';
-import Animated, { FlipInYLeft } from 'react-native-reanimated';
+import Animated, { FadeOut, FlipInYLeft } from 'react-native-reanimated';
 import AuthSubtext from '@components/AuthSubtext';
+import { memo } from 'react';
 
 export default function LoginScreen() {
   return (
     <Animated.View
       entering={FlipInYLeft}
+      exiting={FadeOut}
       testID='Animated View'
       style={{
         alignItems: 'center',
         width: 330,
         flex: 1,
-        borderColor: 'red',
-        borderWidth: 1,
+        // borderColor: 'red',
+        // borderWidth: 1,
       }}
     >
-      <LogIn />
-      <AuthSubtext />
+      <MemoizedLogIn />
+      <MemoizedAuthSubtext />
     </Animated.View>
   );
 }
+
+const MemoizedLogIn = memo(LogIn);
+const MemoizedAuthSubtext = memo(AuthSubtext);
--- app/(userAuth)/signup.tsx
+++ app/(userAuth)/signup.tsx
@@ -1,11 +1,13 @@
 import AuthSubtext from '@components/AuthSubtext';
 import { SignUp } from '@components/SignUp';
-import Animated, { FlipInYRight } from 'react-native-reanimated';
+import { memo } from 'react';
+import Animated, { FadeOut, FlipInYRight } from 'react-native-reanimated';
 
 export default function SignupScreen() {
   return (
     <Animated.View
       entering={FlipInYRight}
+      exiting={FadeOut}
       testID='Animated View'
       style={{
         alignItems: 'center',
@@ -13,8 +15,11 @@ export default function SignupScreen() {
         flex: 1,
       }}
     >
-      <SignUp />
-      <AuthSubtext />
+      <MemoizedSignUp />
+      <MemoizedAuthSubtext />
     </Animated.View>
   );
 }
+
+const MemoizedSignUp = memo(SignUp);
+const MemoizedAuthSubtext = memo(AuthSubtext);
