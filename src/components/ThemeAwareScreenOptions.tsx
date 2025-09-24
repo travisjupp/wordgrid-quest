72f9ba1b9616860c3d7d80911894ffda40e4b540
tjupp <travisjupp@gmail.com>
2025-09-24 12:29:54 -0600

    Replace `ThemeAwareStack` with `ThemeAwareScreenOptions`
    See: Create Theme Aware Screen Options #133 for details
    
    - `ThemeAwareScreenOptions` updated to access the current screens
      navigator options only and returns no UI
    
    - To avoid confusion with nested navigators/routes we've decided against
      using a `ThemeAwareStack`
    
    - `ThemeAwareScreenOptions` was created so each screen could manage their
      own Screen Options but may not be used if it adds too much complexity
      to our navigation structure
    
    Related:
    
      Configure Screen Transition Animations (Web) #135
    
        Configure screen options #113
    
        Configure Screen Transition Animations (Web) #135 (Extending Expo
        Router)

--- src/components/ThemeAwareScreenOptions.tsx
+++ src/components/ThemeAwareScreenOptions.tsx
@@ -1,48 +1,50 @@
-import { withLayoutContext } from 'expo-router';
-import React from 'react';
 import { Platform, View } from 'react-native';
 import { CLogo } from '@components/CLogo';
 import { Menu } from '@components/Menu';
 import { useTheme } from '@hooks/useTheme';
-import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from 'expo-router';
+import { useLayoutEffect } from 'react';
+import CustomHeader from './CustomHeader';
 
 interface Props {
   header?: boolean | undefined;
   menu?: boolean | undefined;
   back?: boolean | undefined;
+  title?: string | undefined;
 }
 
-export function ThemeAwareScreenOptions({ header, menu, back }: Props) {
+export function ThemeAwareScreenOptions({ header = true, menu = false, back, title }: Props) {
+  const navigation = useNavigation(); /* Access current screens navigator */
   const { theme } = useTheme();
 
-  const AnimatedStack = withLayoutContext(createStackNavigator().Navigator);
-  return (
-    <AnimatedStack.Screen
-      options={{
-        headerStyle: Platform.select({
-          web: {
-            borderBottomColor: theme?.colors.outlineVariant,
-            backgroundColor: theme?.colors.surfaceContainer,
-          },
-          default: {
-            backgroundColor: theme?.colors.surfaceContainer,
-          },
-        }),
-        headerTintColor: theme?.colors.onSurface,
-        headerShown: header ? true : false,
-        headerTitleStyle: {
-          fontWeight: 'bold',
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: header,
+      headerStyle: Platform.select({
+        web: {
+          borderBottomColor: theme?.colors.outlineVariant,
+          backgroundColor: theme?.colors.surfaceContainer,
         },
-        headerTitleAlign: 'center',
-        headerBackVisible: back ? true : false,
-        headerLeft: back ? undefined : () => <View />,
-        headerBackButtonDisplayMode: 'minimal',
-        headerTitle: (props: any) => {
-          props.back = back ?? false; /* Pass 'back' as prop */
-          return <CLogo {...props} />;
+        default: {
+          backgroundColor: theme?.colors.surfaceContainer,
         },
-        headerRight: (props: any) => (menu ? <Menu {...props} /> : null),
-      }}
-    />
-  );
+      }),
+      headerTintColor: theme?.colors.onSurface,
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+      headerTitleAlign: 'center',
+      headerBackVisible: back ? true : false,
+      headerLeft: back ? undefined : () => <View />,
+      headerBackButtonDisplayMode: 'minimal',
+      headerTitle: (props: any) => {
+        props.back = back ?? false; /* Pass 'back' as prop */
+        props.title = title ?? false; /* Pass 'title' as prop */
+        return <CLogo {...props} />;
+      },
+      headerRight: (props: any) => (menu ? <Menu {...props} /> : null),
+    });
+  }, [navigation, header, menu, back, title, theme]);
+
+  return null; /* Renders no UI */
 }
