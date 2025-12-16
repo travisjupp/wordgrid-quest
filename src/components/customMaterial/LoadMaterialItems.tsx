34b08878ffb1c3cb6cc671e8a5d35bcfbeaa247f
tjupp <travisjupp@gmail.com>
2025-12-16 15:46:06 -0700

    Disable "+" and "Cancel" buttons when BottomSheet displayed #161
    Related:
    
      - Create a `LoadMaterialItems` component with a Chip displaying our current Category #101

--- src/components/customMaterial/LoadMaterialItems.tsx
+++ src/components/customMaterial/LoadMaterialItems.tsx
@@ -16,7 +16,12 @@ export function LoadMaterialItems() {
   const dispatch = useAppDispatch();
   const tempCategory = useAppSelector(selectTempCustomCategory);
   const router = useRouter();
-  const { showBottomSheet, hideBottomSheet } = useBottomSheetCustom();
+  const {
+    showBottomSheet,
+    hideBottomSheet,
+    snapBottomSheet,
+    expandedBottomSheet,
+  } = useBottomSheetCustom();
   const handleEditCategory = () => {
     hideBottomSheet();
     dispatch(resetTempMaterial());
@@ -28,7 +33,7 @@ export function LoadMaterialItems() {
 
   // Retrieve Custom Theme-properties
   const {
-    colors: { onPrimary },
+    colors: { onPrimary, onSurfaceDisabled },
     shared: { inputWrapper: sharedInputWrapper },
   } = useAppTheme();
 
@@ -37,7 +42,7 @@ export function LoadMaterialItems() {
       <View style={sharedInputWrapper} testID='InputWrapper'>
         <Chip content={tempCategory} onPress={handleEditCategory} />
         <Button
-          onPress={() => showBottomSheet(<LoadItem />)}
+          disabled={expandedBottomSheet ? true : false}
           contentStyle={{
             height: 50,
             justifyContent: 'center',
@@ -47,9 +52,14 @@ export function LoadMaterialItems() {
           style={{ marginTop: 21 }}
           testID='Button Add Material'
         >
-          <Icon source={'plus-circle-outline'} color={onPrimary} size={22} />
+          <Icon
+            source={'plus-circle-outline'}
+            color={expandedBottomSheet ? onSurfaceDisabled : onPrimary}
+            size={22}
+          />
         </Button>
         <Button
+          disabled={expandedBottomSheet ? true : false}
           contentStyle={{ height: 50 }}
           style={{ marginTop: 6 }}
           mode='contained'
