0cd69bd7ca7677fc6d1354b203a319ad70f2da19
tjupp <travisjupp@gmail.com>
2025-12-26 05:51:53 -0700

    build(metro): add external javascript path
    - This is currently needed for logging styles

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,13 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
+const path = require('path');
 const { getDefaultConfig } = require('@expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 config.resolver.sourceExts.push('cjs');
 config.resolver.unstable_enablePackageExports = false;
+const externalPath = path.resolve(__dirname, '../Javascript');
+config.watchFolders = [...(config.watchFolders || []), externalPath];
 
 module.exports = config;
 
