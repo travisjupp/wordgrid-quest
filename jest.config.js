ff40a1142055b10ed69ae8ae22ef132dfab3b86a
tjupp <travisjupp@gmail.com>
2025-12-30 18:11:30 -0700

    dx(jest): refactor transformIgnorePatterns into Array-joined Whitelist
    - Context: The single opaque `transformWhitelist` string makes it
      difficult to audit whitelisted ESM packages and is prone to syntax
      errors when adding entries via regex concatenation.
    - Action: Refactor `transformIgnorePatterns` into an **Array-joined
      Whitelist**
    - Result: Managing ESM package whitelist is easier to manage and less
      prone to syntax errors.
    
    Related: #168

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,31 @@
+const transformWhitelist = [
+  '(jest-)?react-native',
+  '@react-native(-community)?',
+  'expo(nent)?',
+  '@expo(nent)?/.*',
+  '@expo-google-fonts/.*',
+  'react-navigation',
+  '@react-navigation/.*',
+  '@unimodules/.*',
+  'unimodules',
+  'sentry-expo',
+  'native-base',
+  'react-native-svg',
+  'react-redux',
+  '@reduxjs/toolkit',
+  'react-native-keyboard-controller',
+  'react-native-safe-area-context',
+  '@gorhom/bottom-sheet',
+  'react-native-paper',
+  'react-native-screens',
+  'react-native-webview',
+  'firebase',
+  '@expo/vector-icons',
+];
+
 module.exports = {
   preset: 'jest-expo',
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-redux|@reduxjs/toolkit|react-native-keyboard-controller|react-native-safe-area-context|@gorhom/bottom-sheet)'
+    `node_modules/(?!(${transformWhitelist.join('|')}))`,
   ],
-}
+};
